13d982b9709c2f2503d817d9aa2b4cf5e89a3df8
oharek8 <35497829+oharek8@users.noreply.github.com>
2018-09-07 21:55:36 -0400

    Add files via upload

---
+++ Assignment/math.c
@@ -0,0 +1,19 @@
+int math(int num1, int num2, char Operator)
+/*Inputs are the First and Second Integers, as well as the desired operation*/
+{
+    switch(Operator) //A switch statement uses the Operator to decide which function to perform
+    {
+        case '+': return num1 + num2; //Adds Num1 and Num2
+        case '-': return num1 - num2; //Subtracts Num2 from Num1
+        case '*': return num1 * num2; //Multiplies Num1 by Num2
+        case '/': return num1 / num2; //Divides Num1 by Num2
+        case '%': return num1 % num2; //Modulo Divides Num1 by Num2
+        case '<': return num1 << num2;//Binary Shifts Num1 to the Left by amount Num2
+        case '>': return num1 >> num2;//Binary Shifts Num1 to the Right by amount Num2
+        case '&': return num1 & num2; //Bitwise And's Num1 and Num2
+        case '|': return num1 | num2; //Bitwise Or's Num1 and Num2
+        case '^': return num1 ^ num2; //Bitwise Xor's Num1 and Num2
+        case '~': return ~num1;       //Returns Binary inverse of Num1. Also known as the One's Complement
+        default: return 0; //If unknown operator is entered, nothing is done
+    }
+}
